ccbfefc44b5ae63833c8e8e2d5542c8078a8527c
Saeed Ahmadi <saeedahmadi694@gmail.com>
2024-11-11 23:34:07 +0330

    init git

---
+++ SeleniumTest/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SeleniumTest;
+
+Console.WriteLine("Hello, World!");
+var test = new GitHubTests();
+test.SetUp();
+//test.TestLoginToGitHub();
+test.TestCreateRepoBranchAndPullRequest();
